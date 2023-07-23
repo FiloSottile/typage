42f88b04996ded78f0fa774fa05b7f2ed375309b
Filippo Valsorda <hi@filippo.io>
2023-07-23 17:55:57 +0200

    Make eslint actually check useful stuff (?!?!)

---
+++ .eslintrc.cjs
@@ -0,0 +1,15 @@
+module.exports = {
+    ignorePatterns: ['.eslintrc.cjs'],
+    extends: [
+        'eslint:recommended',
+        'plugin:@typescript-eslint/strict-type-checked',
+        'plugin:@typescript-eslint/stylistic-type-checked',
+    ],
+    plugins: ['@typescript-eslint'],
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+        project: true,
+        tsconfigRootDir: __dirname,
+    },
+    root: true,
+};
