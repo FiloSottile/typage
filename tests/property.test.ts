132c95e84cf507a6768b0406b591764e4177970f
Sebastian <olastor@users.noreply.github.com>
2023-08-24 21:59:56 +0200

    chore: add property tests for encryption/decryption

---
+++ tests/property.test.ts
@@ -0,0 +1,117 @@
+import { describe } from 'vitest'
+import { test, fc } from '@fast-check/vitest'
+import age from '../lib/index.js'
+import { to_string } from 'libsodium-wrappers-sumo'
+
+fc.configureGlobal({
+  // increasing this value will make fast-check do more random test runs,
+  // which might be good for intensive testing, but also makes them slower.
+  numRuns: 50
+})
+
+const isEqualUInt8Array = (a: Uint8Array, b: Uint8Array) => {
+  if (a.length !== a.length) {
+    return false
+  }
+
+  for (let i = 0; i < a.byteLength; i++) {
+    if (a[i] !== b[i]) {
+      return false
+    }
+  }
+
+  return true
+}
+
+describe('Property Based Tests', () => {
+  describe('Asymmetric Encryption and Decryption', () => {
+    test.prop(
+      {
+        plaintext: fc.string()
+      }
+    )(
+      'decryption should invert encryption with identity/recipient (string plaintext)',
+      async ({ plaintext }) => {
+        const { Decrypter, Encrypter, generateIdentity, identityToRecipient } = await age()
+
+        const identity = generateIdentity()
+        const recipient = identityToRecipient(identity)
+
+        const enc = new Encrypter()
+        const dec = new Decrypter()
+
+        enc.addRecipient(recipient)
+        dec.addIdentity(identity)
+
+        return to_string(dec.decrypt(enc.encrypt(plaintext))) === plaintext
+      }
+    )
+
+    test.prop(
+      {
+        plaintext: fc.uint8Array()
+      }
+    )(
+      'decryption should invert encryption with identity/recipient (uint8array plaintext)',
+      async ({ plaintext }) => {
+        const { Decrypter, Encrypter, generateIdentity, identityToRecipient } = await age()
+
+        const identity = generateIdentity()
+        const recipient = identityToRecipient(identity)
+
+        const enc = new Encrypter()
+        const dec = new Decrypter()
+
+        enc.addRecipient(recipient)
+        dec.addIdentity(identity)
+
+        return isEqualUInt8Array(dec.decrypt(enc.encrypt(plaintext)), plaintext)
+      }
+    )
+  })
+
+
+  describe('Symmetric Encryption and Decryption', () => {
+    test.prop(
+      {
+        plaintext: fc.string(),
+        passphrase: fc.string(),
+        scryptWorkFactor: fc.integer({ min: 1, max: 12 })
+      }
+    )(
+      'decryption should invert encryption with passphrase (string plaintext)',
+      async ({ plaintext, passphrase, scryptWorkFactor }) => {
+        const { Decrypter, Encrypter } = await age()
+        const enc = new Encrypter()
+        const dec = new Decrypter()
+
+        enc.setScryptWorkFactor(scryptWorkFactor)
+        enc.setPassphrase(passphrase)
+        dec.addPassphrase(passphrase)
+
+        return to_string(dec.decrypt(enc.encrypt(plaintext))) === plaintext
+      }
+    )
+
+    test.prop(
+      {
+        plaintext: fc.uint8Array(),
+        passphrase: fc.string(),
+        scryptWorkFactor: fc.integer({ min: 1, max: 12 })
+      }
+    )(
+      'decryption should invert encryption with passphrase (UInt8Array plaintext)',
+      async ({ plaintext, passphrase, scryptWorkFactor }) => {
+        const { Decrypter, Encrypter } = await age()
+        const enc = new Encrypter()
+        const dec = new Decrypter()
+
+        enc.setScryptWorkFactor(scryptWorkFactor)
+        enc.setPassphrase(passphrase)
+        dec.addPassphrase(passphrase)
+
+        return isEqualUInt8Array(dec.decrypt(enc.encrypt(plaintext)), plaintext)
+      }
+    )
+  })
+})
