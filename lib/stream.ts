16bc44e8693804743043ed4e9f5b97d966543259
Filippo Valsorda <hi@filippo.io>
2023-11-03 10:28:28 +0100

    Import libsodium-wrappers-sumo via the default export
    Fixes Node module resolution.
    
    Partially based on #11 by @lgarron.
    
    Fixes #9

--- lib/stream.ts
+++ lib/stream.ts
@@ -1,4 +1,4 @@
-import * as sodium from "libsodium-wrappers-sumo"
+import sodium from "libsodium-wrappers-sumo"
 
 // We can't use sodium.crypto_aead_chacha20poly1305_IETF_ABYTES here before
 // sodium.ready, or it will make the constant be silently NaN, and nothing will
